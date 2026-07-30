a6377e6b1363289577173ae5225ed47775e4c9df
Senol Gulgonul <63923646+senolgulgonul@users.noreply.github.com>
2026-07-30 14:39:54 +0300

    Delete sekiz-toolchain directory

--- sekiz-toolchain/scripts/build-native-tblgen.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Stage 1: native llvm-tblgen + clang-tblgen (needed to cross-build LLVM to WASM)
-set -e
-cmake -G Ninja -S llvm/llvm -B build-native \
-  -DCMAKE_BUILD_TYPE=Release \
-  -DLLVM_ENABLE_PROJECTS="clang" \
-  -DLLVM_TARGETS_TO_BUILD="AVR" \
-  -DLLVM_INCLUDE_TESTS=OFF -DLLVM_INCLUDE_EXAMPLES=OFF -DLLVM_INCLUDE_BENCHMARKS=OFF
-ninja -C build-native llvm-tblgen clang-tblgen
